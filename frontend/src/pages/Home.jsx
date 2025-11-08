96585458378064a6de17bc95dbbf636e41dbe28c
Sadhna Kumari <ss.sadhna263@gmail.com>
2025-11-09 01:37:38 +0530

    Footer Done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
 import AiTools from '../components/AiTools';
 import Testimonial from '../components/Testimonial';
+import Footer from '../components/Footer';
 
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
       <Hero />
       <AiTools />
       <Testimonial />
+      <Footer />
     </>
   );
 }
