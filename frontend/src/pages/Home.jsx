4fa414459ebce6178a7d0a9852f5404f2c89d377
Sadhna Kumari <ss.sadhna263@gmail.com>
2025-11-08 23:51:58 +0530

    Hero section + ai tools + testimonial DONE

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
+import AiTools from '../components/AiTools';
+import Testimonial from '../components/Testimonial';
 
 
 const Home = () => {
@@ -8,6 +10,8 @@ const Home = () => {
     <>
       <Navbar />
       <Hero />
+      <AiTools />
+      <Testimonial />
     </>
   );
 }
