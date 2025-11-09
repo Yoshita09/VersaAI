bf141f15d385a9b999f663a995cff5ba4ce7e1f0
Yoshita09 <yoshitasinghal.2005@gmail.com>
2025-11-09 05:56:34 +0530

    Upload hover added

--- frontend/src/pages/RemoveObject.jsx
+++ frontend/src/pages/RemoveObject.jsx
@@ -30,7 +30,7 @@ const RemoveObject = () => {
           onChange={(e) => setInput(e.target.files[0])}
           type="file"
           accept="image/*"
-          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600 cursor-pointer"
           required
         />
 
